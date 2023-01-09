7661e318f1fbe98dd31020e2a5dd7c1589460334
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-09 17:01:26 +0530

    Commit

--- src/main/java/com/lti/springboot/demo/controller/HelloController.java
+++ src/main/java/com/lti/springboot/demo/controller/HelloController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+//	http://localhost:9090/hello
+
 	@RequestMapping("/hello")
 	public String hello() {
 		System.out.println("Hello");
