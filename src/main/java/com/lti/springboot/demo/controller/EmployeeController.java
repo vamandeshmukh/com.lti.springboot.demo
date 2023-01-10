951d1f52cc84942aea3688fcf80eb2663343c3a3
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-10 15:36:23 +0530

    spring-boot

--- src/main/java/com/lti/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/lti/springboot/demo/controller/EmployeeController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lti.springboot.demo.model.Employee;
@@ -19,7 +21,8 @@ public class EmployeeController {
 	@Autowired
 	private IEmployeeService empService;
 
-	@RequestMapping("get-all-emps")
+	@RequestMapping(path = "get-all-emps", method = RequestMethod.GET)
+//	@GetMapping("get-all-emps")
 	public List<Employee> getAllEmps() {
 		LOG.info("getAllEmps");
 		return empService.getAllEmployees();
