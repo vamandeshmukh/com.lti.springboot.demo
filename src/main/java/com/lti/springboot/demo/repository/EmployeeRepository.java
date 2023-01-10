65f782877e8955feef76bd7d98988f4a7a270b61
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-10 16:52:44 +0530

    spring-boot

--- src/main/java/com/lti/springboot/demo/repository/EmployeeRepository.java
+++ src/main/java/com/lti/springboot/demo/repository/EmployeeRepository.java
@@ -1,5 +1,7 @@
 package com.lti.springboot.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,10 +9,20 @@ import com.lti.springboot.demo.model.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-	
-//	findById();
-//	findAll();
-	
-	
+
+//	documentation : https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repository-query-keywords 
+
+//  for basic CRUD ops, methods are already available;
+//  for business specific ops, methods need to be declared here as below - 
+
+//	public abstract List<EntityClass> findByFieldName(type fieldName);
+
+	public abstract List<Employee> findBySalary(double salary);
+
+	public abstract List<Employee> findBySalaryGreaterThan(double salary);
+
+	public abstract List<Employee> findBySalaryLessThan(double salary);
+
+	public abstract List<Employee> findBySalaryBetween(double fromSalary, double toSalary);
 
 }
