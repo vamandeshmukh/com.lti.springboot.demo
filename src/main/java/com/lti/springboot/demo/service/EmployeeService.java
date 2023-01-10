ce66ecea78e200657ab918abeab4520c9117eee6
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-10 11:15:31 +0530

    spring-boot

--- src/main/java/com/lti/springboot/demo/service/EmployeeService.java
+++ src/main/java/com/lti/springboot/demo/service/EmployeeService.java
@@ -1,6 +1,18 @@
 package com.lti.springboot.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.lti.springboot.demo.model.Employee;
+
 public class EmployeeService {
 
-}
+	public List<Employee> getAllEmployees() {
+		List<Employee> empList = new ArrayList<>();
+		empList.add(new Employee(101, "Sonu", 90000));
+		empList.add(new Employee(102, "Monu", 99000));
+		empList.add(new Employee(103, "Tonu", 95000));
+		return empList;
+	}
 
+}
