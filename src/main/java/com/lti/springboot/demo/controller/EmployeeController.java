faf94e8080b623da9c2eb879a247a86b22808047
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-10 11:11:33 +0530

    spring-boot

--- src/main/java/com/lti/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/lti/springboot/demo/controller/EmployeeController.java
@@ -1,6 +1,23 @@
 package com.lti.springboot.demo.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lti.springboot.demo.model.Employee;
+
+@RestController
 public class EmployeeController {
 
-}
+	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
+	@RequestMapping("get-all-emps")
+	public List<Employee> getAllEmps() {
+		LOG.info("getAllEmps");
+		return null;
+	}
+
+}
